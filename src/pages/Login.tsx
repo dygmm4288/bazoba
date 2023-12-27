52034d682ec7630c795fb77b4dd8d59cb69b2e20
jjuhee <azaza.hee@gmail.com>
2023-12-27 17:37:55 +0900

    [ADD] supabase database type파일 생성

---
+++ src/pages/Login.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../supabase';
+
+const Login = () => {
+  //   const [data, setData] = useState<any>();
+  //   useEffect(() => {
+  //     const fetchData = async () => {
+  //       const { data, error } = await supabase.from('post').select('*');
+  //       console.log(data);
+  //       setData(data);
+  //     };
+  //     fetchData();
+  //   }, []);
+
+  return <div>Login</div>;
+};
+
+export default Login;
--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from '../pages/Login';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<div>Home</div>} />
-        <Route path='/detail' element={<div>detail page</div>} />
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/detail" element={<div>detail page</div>} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
