9478f20e21cc010554902370a4e49724ec156b05
Yongseung Song <yongseung.song@icloud.com>
2023-12-27 21:17:57 +0900

    [FIX] Conflict 해결

--- src/supabase/index.tsx
+++ src/supabase/index.tsx
@@ -3,7 +3,7 @@ import { BookmarkType, LikeType, PostType } from './supabase.types';
 import { Database } from './supabaseSchema.types';
 
 export const db = createClient<Database>(
-  process.env.REACT_APP_SUPABASE_URL!,
+  process.env.REACT_APP_SUPABASE_API_KEY!,
   process.env.REACT_APP_SUPABASE_API_KEY!
 );
 
