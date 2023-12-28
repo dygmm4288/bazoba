af7a5139e94315ca30183d3036322391acddfd4f
HyoChang5147 <143697280+HyoChang5147@users.noreply.github.com>
2023-12-28 18:01:56 +0900

    detail 댓글 추가기능 만드는중

--- src/components/Detail/DetailFormComment.tsx
+++ src/components/Detail/DetailFormComment.tsx
@@ -1,33 +1,7 @@
-import { useState } from 'react';
-import { addComment } from '../../supabase/supabase.types';
+import React from 'react';
 
-const DetailFormComment = () => {
-  const [comment, setComment] = useState('');
-
-  const handleAddComment = () => {
-    if (comment.trim() === '') {
-      alert('댓글을 입력하세요.');
-      return;
-    }
-
-    const newComment = {
-      content: comment
-    };
-
-    addComment('postId', newComment);
-    setComment('');
-  };
-
-  return (
-    <div>
-      <textarea
-        value={comment}
-        onChange={(e) => setComment(e.target.value)}
-        placeholder="댓글을 입력해 주세요"
-      />
-      <button onClick={handleAddComment}>댓글 추가</button>
-    </div>
-  );
-};
+function DetailFormComment() {
+  return <div>DetailFormComment</div>;
+}
 
 export default DetailFormComment;
