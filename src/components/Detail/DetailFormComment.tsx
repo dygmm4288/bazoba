775f40661e40084a1df2ef0f66d316942c4e82d9
HyoChang5147 <143697280+HyoChang5147@users.noreply.github.com>
2023-12-28 20:34:41 +0900

    댓글 추가 기능

--- src/components/Detail/DetailFormComment.tsx
+++ src/components/Detail/DetailFormComment.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAddComment } from '../../hooks/useSupabase';
+
+import type { CommentType } from '../../supabase/supabase.types';
 
 function DetailFormComment() {
-  return <div>DetailFormComment</div>;
+  const [commentContent, setCommentContent] = useState('');
+  const { addComment } = useAddComment('a38a1eb0-e793-472b-9f9a-f5dbd608afa8');
+
+  const handleAddComment = () => {
+    if (commentContent.trim() !== '') {
+      const newComment: CommentType = {
+        id: '',
+        content: commentContent,
+        postId: 'a38a1eb0-e793-472b-9f9a-f5dbd608afa8'
+      };
+      addComment(newComment);
+      setCommentContent('');
+    }
+  };
+
+  return (
+    <div>
+      <h2>댓글 작성하기</h2>
+      <textarea
+        rows={4}
+        cols={50}
+        value={commentContent}
+        onChange={(e) => setCommentContent(e.target.value)}
+        placeholder="Type your comment here..."
+      />
+      <br />
+      <button onClick={handleAddComment}>댓글 등록</button>
+    </div>
+  );
 }
 
 export default DetailFormComment;
