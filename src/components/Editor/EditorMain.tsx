ac4da6270a57ee7ee2d41c2718b26156ef9e924b
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-12-29 17:58:44 +0900

    [REFACTOR] uploadImage 를 이용한 코드 간소화

--- src/components/Editor/EditorMain.tsx
+++ src/components/Editor/EditorMain.tsx
@@ -3,7 +3,7 @@ import { HookCallback } from '@toast-ui/editor/types/editor';
 import { Editor } from '@toast-ui/react-editor';
 import { Content } from 'antd/es/layout/layout';
 import { EditorRefType } from '../../hooks/useEditorForm';
-import { db } from '../../supabase';
+import { uploadImage } from '../../supabase';
 
 interface Props {
   editorRef: EditorRefType;
@@ -11,16 +11,12 @@ interface Props {
 
 export default function EditorMain({ editorRef }: Props) {
   const handleImageUpload = async (blob: Blob | File, put: HookCallback) => {
-    const URL =
-      'https://borxwimnmhmdodedkkpv.supabase.co/storage/v1/object/public/post_images/';
-    const { data, error } = await db.storage
-      .from('post_images')
-      .upload(window.URL.createObjectURL(blob), blob);
+    const { data, error } = await uploadImage(blob);
     if (error) {
       console.error(error);
-    } else {
-      put(URL + data.path);
+      return;
     }
+    put(data);
   };
 
   return (
